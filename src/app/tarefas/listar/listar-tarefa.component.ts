034fb4c5ab71ec8a5413339935608e30ceb70a6a
Julia Pereira <juliapereira.odc@gmail.com>
2022-03-08 15:04:41 -0300

    add remover tarefas

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -4,20 +4,26 @@ import { TarefaService, Tarefa } from '../shared';
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
-  styleUrls: ['./listar-tarefa.component.css']
+  styleUrls: ['./listar-tarefa.component.css'],
 })
 export class ListarTarefaComponent implements OnInit {
-
   tarefas: Tarefa[];
 
-  constructor(private tarefaService: TarefaService) { }
+  constructor(private tarefaService: TarefaService) {}
 
   ngOnInit(): void {
     this.tarefas = this.listarTodos();
   }
 
-  listarTodos(): Tarefa[]{
+  listarTodos(): Tarefa[] {
     return this.tarefaService.listarTodos();
   }
 
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
 }
